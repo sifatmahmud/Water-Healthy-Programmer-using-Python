69b5ec170b1984bce0bc5043bb93f6226f2f7b72
sifat <sifatmahmud5293@gmail.com>
2021-02-19 17:45:35 +0600

    add a if condition in while loop in main function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def musicon(file, stopper):
     mixer.music.load(file)
     mixer.music.play()
     while True:
-        drank_input = input("Type drank to stop : ")
+        drank_input = input("\nType drank to stop : ")
         if drank_input == stopper:
             mixer.music.stop()
             break
@@ -18,15 +18,18 @@ def musicon(file, stopper):
             continue
 
 
-drank_time = datetime.datetime.now()
+if __name__ == '__main__':
+    drank_time = datetime.datetime.now()
 
-loop_count = 0
-while loop_count < 14:
-    program_sleeper = time.sleep(2280)
-    musicon("DJ.mp3", "drank")
-    print(f"you have drank water at : {drank_time}")
-    loop_count += 1
+    loop_count = 0
+    while loop_count < 14:
+        if loop_count == 0:
+            print("( Welcome to the water acitvity, Be good - Be healthy )")
+        program_sleeper = time.sleep(2)
+        musicon("DJ.mp3", "drank")
+        print(f"you have drank water at : {drank_time}")
+        loop_count += 1
 
-water_activity = open("water_activity_file.txt", "a")
-water_activity.write(f"\n{drank_time} : drank")
-water_activity.close()
+    water_activity = open("water_activity_file.txt", "a")
+    water_activity.write(f"\n{drank_time} : drank")
+    water_activity.close()
